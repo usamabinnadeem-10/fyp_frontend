a2208bf4a332b040cdf3ea3d8e069e3e38d80342
usamabinnadeem-10 <32700508+usamabinnadeem-10@users.noreply.github.com>
2021-05-18 23:59:20 +0500

    bug fix in response

--- src/components/CarGallery/CarGallery.js
+++ src/components/CarGallery/CarGallery.js
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function CarGallery ({imagesList}) {
-  const [receiveImagesList,setReceiveImagesList] = useState({});
+function CarGallery({ imagesList }) {
+  const [receiveImagesList, setReceiveImagesList] = useState({});
   const history = useHistory();
 
-  useEffect(()=>{
-    if(imagesList && (!imagesList.names)){
-      history.push('/');
+  useEffect(() => {
+    console.log(imagesList);
+    if (imagesList && !imagesList.names) {
+      history.push("/");
     }
     setReceiveImagesList(imagesList);
-    console.log(receiveImagesList);
-  },[receiveImagesList,imagesList,history])
+  }, [receiveImagesList, imagesList, history]);
 
-  
   return (
-    <div style={{ height: '100vh', width: '100%' }} className="container">
-      {
-        receiveImagesList && receiveImagesList.images &&  Object.keys(receiveImagesList.images).map((img)=>(
-          <img src={`data:image/jpg;base64,${receiveImagesList.images[img]}`} key={receiveImagesList.names[img]} alt={receiveImagesList.names[img]}/>
-        ))
-      }
+    <div style={{ height: "100vh", width: "100%" }} className="container">
+      {receiveImagesList &&
+        receiveImagesList.images &&
+        Object.keys(receiveImagesList.images).map((img) => (
+          <img
+            src={`data:image/jpg;base64,${receiveImagesList.images[img]}`}
+            key={receiveImagesList.names[img]}
+            alt={receiveImagesList.names[img]}
+          />
+        ))}
     </div>
   );
 }
- 
+
 export default CarGallery;
--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -1,70 +1,72 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Button, Dimmer, Icon, Loader, Segment } from 'semantic-ui-react'
-import axios from 'axios';
+import { Button, Dimmer, Icon, Loader, Segment } from "semantic-ui-react";
+import axios from "axios";
 
-import './UploadImage.css';
+import "./UploadImage.css";
 
 export default function UploadImage(props) {
-    const [activeLoader,setActiveLoader] = useState(false);
-    const history = useHistory();
-    console.log(props);
+  const [activeLoader, setActiveLoader] = useState(false);
+  const history = useHistory();
+  console.log(props);
 
-    const handleClick = ()=>{
-        document.getElementById('car-img').click();
-        /* setTimeout(()=>{   
+  const handleClick = () => {
+    document.getElementById("car-img").click();
+    /* setTimeout(()=>{   
             setActiveLoader(false);
             history.push('/checking');
         },4000); */
+  };
 
-    }
+  const toBase64 = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async function () {
+      setActiveLoader(true);
+      const images = await axios.post("http://localhost:8000/api/query/", {
+        query: reader.result,
+      });
+      props.handleImageSet(images.data);
+      history.push("/locatedCar");
+      setActiveLoader(false);
+    };
+    reader.onerror = function (error) {
+      console.log("Error: ", error);
+    };
+  };
+  const handleImageUpload = async (event) => {
+    event.preventDefault();
+    setActiveLoader(true);
+    const file = event.target.files[0];
+    toBase64(file);
+  };
 
+  return (
+    <div className="row upload-image-page">
+      <Segment basic className="upload-image-page">
+        <Dimmer active={activeLoader} inverted>
+          <Loader inverted content="Running Your Image Through Our Network" />
+        </Dimmer>
 
-    const toBase64 = (file) =>{
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = async function () {
-            setActiveLoader(true);
-            const images = await axios.post("http://localhost:8000/api/query/",{
-                    "query":reader.result
-                });
-            props.handleImageSet(images.data.result);
-            history.push('/locatedCar')
-            setActiveLoader(false); 
-        };
-        reader.onerror = function (error) {
-            console.log('Error: ', error);
-        };
-    }
-    const handleImageUpload = async (event)=>{
-        event.preventDefault();
-        setActiveLoader(true);
-        const file = event.target.files[0];
-        toBase64(file);
-    }
-
-    return (
-        <div className="row upload-image-page">
-            <Segment basic className='upload-image-page'>
-                <Dimmer active={activeLoader} inverted>
-                    <Loader inverted content='Running Your Image Through Our Network' />
-                </Dimmer>
-
-                <Button animated onClick={handleClick}>
-                    <Button.Content>
-                        <input type="file" id="car-img" name="car-img" accept="image/*" placeholder="Upload Image" style={{display:'none'}} onChange={event=>handleImageUpload(event)}/>
-                        <Button.Content>Upload Image <Icon name='upload right'/></Button.Content>
-                    </Button.Content>
-                </Button>
-                <p>
-                    Please upload the image of your query car.
-                </p>
-                <p>
-                    Dankeschon!
-                </p>
-                            
-            </Segment>
-                   
-        </div>
-    )
+        <Button animated onClick={handleClick}>
+          <Button.Content>
+            <input
+              type="file"
+              id="car-img"
+              name="car-img"
+              accept="image/*"
+              placeholder="Upload Image"
+              style={{ display: "none" }}
+              onChange={(event) => handleImageUpload(event)}
+            />
+            <Button.Content>
+              Upload Image <Icon name="upload right" />
+            </Button.Content>
+          </Button.Content>
+        </Button>
+        <p>Please upload the image of your query car.</p>
+        <p>Dankeschon!</p>
+      </Segment>
+    </div>
+  );
 }
