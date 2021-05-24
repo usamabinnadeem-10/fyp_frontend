8f3601f30fdd14d904a3d7bbd38b8569230e20d4
usamabinnadeem-10 <32700508+usamabinnadeem-10@users.noreply.github.com>
2021-05-24 17:32:41 +0500

    removed Dankeshon

--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -1,15 +1,22 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Button, Dimmer, Icon, Loader, Message, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Dimmer,
+  Icon,
+  Loader,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 import axios from "axios";
 
 import "./UploadImage.css";
 
 export default function UploadImage(props) {
   const [activeLoader, setActiveLoader] = useState(false);
-  const [queryImage,setQueryImage]= useState("");
-  const [fileSizeError,setFileSizeError] = useState(false);
-  const [fileTypeError,setFileTypeError] = useState(false);
+  const [queryImage, setQueryImage] = useState("");
+  const [fileSizeError, setFileSizeError] = useState(false);
+  const [fileTypeError, setFileTypeError] = useState(false);
   const history = useHistory();
   console.log(props);
 
@@ -33,8 +40,7 @@ export default function UploadImage(props) {
     };
   };
 
-
-  const handleProceedImage = async ()=> {
+  const handleProceedImage = async () => {
     setActiveLoader(true);
     const images = await axios.post("http://localhost:8000/api/query/", {
       query: queryImage,
@@ -42,21 +48,20 @@ export default function UploadImage(props) {
     props.handleImageSet(images.data);
     history.push("/locatedCar");
     setActiveLoader(false);
-  }
-
+  };
 
   const handleImageUpload = async (event) => {
     event.preventDefault();
     const file = event.target.files[0];
-    console.log(file.type.includes('image'));
-    if(file.size < 2086803 && file.type.includes('image')){
+    console.log(file.type.includes("image"));
+    if (file.size < 2086803 && file.type.includes("image")) {
       toBase64(file);
     }
-    if(file.size >= 2086803) {
+    if (file.size >= 2086803) {
       setFileSizeError(true);
       console.log("file size is greater than 2mb");
     }
-    if(!(file.type.includes('image'))) {
+    if (!file.type.includes("image")) {
       setFileTypeError(true);
       console.log("file type must be image");
     }
@@ -68,45 +73,56 @@ export default function UploadImage(props) {
         <Dimmer active={activeLoader} inverted>
           <Loader inverted content="Running Your Image Through Our Network" />
         </Dimmer>
-        {queryImage ?<img src={queryImage} alt="queryImage" width="300" height="300"/>:null } 
-        {(!queryImage)? <Button animated onClick={handleClick}>
-          <Button.Content>
-            <input
-              type="file"
-              id="car-img"
-              name="car-img"
-              accept="image/*"
-              placeholder="Upload Image"
-              style={{ display: "none" }}
-              onChange={(event) => handleImageUpload(event)}
-            />
+        {queryImage ? (
+          <img src={queryImage} alt="queryImage" width="300" height="300" />
+        ) : null}
+        {!queryImage ? (
+          <Button animated onClick={handleClick}>
             <Button.Content>
-              Upload Image <Icon name="upload right" />
+              <input
+                type="file"
+                id="car-img"
+                name="car-img"
+                accept="image/*"
+                placeholder="Upload Image"
+                style={{ display: "none" }}
+                onChange={(event) => handleImageUpload(event)}
+              />
+              <Button.Content>
+                Upload Image <Icon name="upload right" />
+              </Button.Content>
             </Button.Content>
-          </Button.Content>
-        </Button>:null}
+          </Button>
+        ) : null}
         <div>
-          {queryImage? 
+          {queryImage ? (
             <>
               <Button attached="left" onClick={handleProceedImage}>
                 Proceed <Icon name="right arrow" />
-              </Button> 
-              <Button attached="right" onClick={()=>{setQueryImage("");props.handleQueryImage("");}}>
+              </Button>
+              <Button
+                attached="right"
+                onClick={() => {
+                  setQueryImage("");
+                  props.handleQueryImage("");
+                }}
+              >
                 Cancel
               </Button>
             </>
-              :null}
+          ) : null}
         </div>
         <p>Please upload the image of your query car.</p>
-        <p>Dankeschon!</p>
-        {  fileSizeError && 
-        <Message negative>
-          <Message.Header>File Size Must be Less Than 2MB</Message.Header>
-        </Message>}
-        {  fileTypeError && 
-        <Message negative>
-          <Message.Header>File Type Must be Image</Message.Header>
-        </Message>}
+        {fileSizeError && (
+          <Message negative>
+            <Message.Header>File Size Must be Less Than 2MB</Message.Header>
+          </Message>
+        )}
+        {fileTypeError && (
+          <Message negative>
+            <Message.Header>File Type Must be Image</Message.Header>
+          </Message>
+        )}
       </Segment>
     </div>
   );
